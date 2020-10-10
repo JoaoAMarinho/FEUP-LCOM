cf090ddf5dbebeb0bd51b4164cba0258dad22373
Joao Marinho <up201905952@fe.up.pt>
2020-10-10 22:51:04 +0000

    Caralho nando passei na primeira funcao

--- lab2/timer.c
+++ lab2/timer.c
@@ -34,7 +34,7 @@ void (timer_int_handler)() {
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
   uint8_t RB_command = TIMER_RB_CMD| TIMER_RB_COUNT_  | TIMER_RB_SEL(timer);
   sys_outb(TIMER_CTRL,RB_command);
-  if(util_sys_inb(TIMER_0+timer,st)!=0) return 1;
+  if(util_sys_inb(TIMER_0+timer,st)) return 1;
 
   return 0;
 }
@@ -44,33 +44,28 @@ int (timer_display_conf)(uint8_t timer, uint8_t st,
   union timer_status_field_val conf;
   switch(field){
     /*!< status */
-    case tsf_all: conf.byte=st;
+    case tsf_all:
+      conf.byte=st;
+      break;
 
     /*!< initialization mode */
-    case tsf_initial:switch(st&48){
-      case 0:conf.in_mode=INVAL_val;
-      case 16:conf.in_mode=LSB_only;
-      case 32:conf.in_mode=MSB_only;
-      case 48:conf.in_mode=MSB_after_LSB;
-    }
+    case tsf_initial:
+      conf.in_mode=(st&TIMER_LSB_MSB)>>4;
+      break;
     
     /*!< counting mode: 0, 1,.., 5 */
-    case tsf_mode:switch(st&14){
-      case 0:conf.count_mode=0;
-      case 2:conf.count_mode=1;
-      case 4:
-      case 12:
-      conf.count_mode=2;
-      case 6:
-      case 14:
-      conf.count_mode=3;
-      case 8:conf.count_mode=4;
-      case 10:conf.count_mode=5;
-    }
+    case tsf_mode:
+      st=(st&(TIMER_SQR_WAVE|BIT(3)))>>1;
+      if(st==6||st==7) st-=4;
+      conf.count_mode=st;
+      break;
 
     /*!< counting base, true if BCD */
-    case tsf_base: conf.bcd=st&1;
+    case tsf_base: 
+      conf.bcd=st&1;
+      break;
   }
-  if(!timer_print_config(timer, field, conf))return 1;
+  if(timer_print_config(timer, field, conf))return 1;
+
   return 0;
 }
