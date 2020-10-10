3f2a02579abb9999beb24f49597171e5614ac851
Joao Marinho <up201905952@fe.up.pt>
2020-10-10 14:08:12 +0000

    New changes to lab2

--- lab2/utils.c
+++ lab2/utils.c
@@ -3,23 +3,23 @@
 #include <stdint.h>
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  if(lsb==NULL)return 1;
 
-  return 1;
+  *lsb=(uint8_t)val;
+  return 0;
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  if(msb==NULL)return 1;
 
-  return 1;
+  *msb=(uint8_t)((val>>8)&0x00FF);
+  return 0;
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
   uint32_t i32;
-  sys_inb(port,&i32);
+  if(sys_inb(port,&i32)!=0)return 1;
+  
   *value=(uint8_t)i32;
-
   return 0;
 }
