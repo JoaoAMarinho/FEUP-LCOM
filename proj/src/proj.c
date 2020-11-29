aff2b4bf4b93d37b467a844f6f7f33c0ec2bc423
up201905952 <up201905952@fe.up.pt>
2020-11-29 13:37:24 +0000

    J!

--- proj/src/proj.c
+++ proj/src/proj.c
@@ -1,18 +1,17 @@
+// IMPORTANT: you must include the following line in all your C files
 #include <lcom/lcf.h>
+#include <lcom/liblm.h>
+#include <lcom/proj.h>
 
-#include <lcom/lab5.h>
-
+#include <stdbool.h>
 #include <stdint.h>
-#include <stdio.h>
 
+// Any header files included below this line should have been created by you
 #include "videocard.h"
 #include "keyboard.h"
 #include "sprite.h"
 #include "i8254.h"
 #include "Player.h"
-#include "proj.h"
-
-// Any header files included below this line should have been created by you
 
 int main(int argc, char *argv[]) {
   // sets the language of LCF messages (can be either EN-US or PT-PT)
@@ -20,11 +19,11 @@ int main(int argc, char *argv[]) {
 
   // enables to log function invocations that are being "wrapped" by LCF
   // [comment this out if you don't want/need it]
-  lcf_trace_calls("/home/lcom/labs/proj/trace.txt");
+  lcf_trace_calls("/home/lcom/labs/proj/src/trace.txt");
 
   // enables to save the output of printf function calls on a file
   // [comment this out if you don't want/need it]
-  lcf_log_output("/home/lcom/labs/proj/output.txt");
+  lcf_log_output("/home/lcom/labs/proj/src/output.txt");
 
   // handles control over to LCF
   // [LCF handles command line arguments and invokes the right function]
@@ -89,3 +88,5 @@ int(proj_main_loop)(int UNUSED(argc), char *UNUSED(argv[])){
 
     return 0;
 }
+
+
--- proj/src/proj.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROJ_H
-#define PROJ_H
-
-
-/**
- * @}
- */
-
-#endif /*PROJ_H*/
